d23e0c285a8082eb814cb754243aa390582e269f
sol namgung <nandaras0103@gmail.com>
2023-01-28 19:09:59 +0900

    big o notation

--- Java/Introduction/Big_O/Ex_01.java
+++ Java/Introduction/Big_O/Ex_01.java
@@ -1,5 +1,11 @@
 package Big_O;
 
+/*
+ * Big O time is the language and metric we use to describe the efficiency of algorithms. 
+ * Not understanding it thoroughly can really hurt you in developing an algorithm. 
+ * Not only might you be judged harshly for not really understanding big 0, 
+ * but you will also struggle to judge when your algorithm is getting faster or slower.
+ */
 public class Ex_01 {
 	
 	public static int sum(int n) {
